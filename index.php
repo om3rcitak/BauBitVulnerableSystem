45c8a3e62d11014b0aa93133b106974ad23ff67a
Om3rCitak <mail@omercitak.com>
2015-11-11 04:19:35 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php require "header.php"; ?>
+
+		<div class="container well">
+			<h2 class="text-center">BAUN Bilgisayar ve İnternet Topluluğu Üye Yönetim Sistemi</h2>
+			<h4 class="text-center">Merhaba hoşgeldiniz. Lütfen üye iseniz giriş yapınız değil ise üye olunuz</h4>
+			<ul>
+				<li><a href="girisyap.php">Giriş Yap</a></li>
+				<li><a href="uyeol.php">Üye Ol</a></li>
+			</ul>
+		</div>
+		
+<?php require "footer.php"; ?>
---
+++ uyeler.php
@@ -0,0 +1,32 @@
+<?php require "header.php"; ?>
+
+		<div class="container well">
+			<h2 class="text-center">BAUN Bilgisayar ve İnternet Topluluğu Üye Yönetim Sistemi</h2>
+			<h4 class="text-center">Merhaba hoşgeldiniz. Lütfen üye iseniz giriş yapınız değil ise üye olunuz</h4>
+			
+			<?php
+			if(!isset($_SESSION["login"])){
+				echo '<div class="alert alert-danger" style="margin-top:50px;"><strong>Opps!</strong> Bu sayfayı görüntüleyebilmek için giriş yapmalısınız</div>';
+				die();
+			}			
+			?>
+			
+			<div class="container" style="margin-top:50px;">
+				<div class="row">
+					<?php
+					$rows = $db->get_results("SELECT * FROM users");
+					foreach($rows AS $row){
+					?>
+					<div class="col-lg-2 col-md-2 col-xs-2 thumb">
+						<a class="thumbnail">
+							<img class="img-responsive" src="resimler/<?php echo $row->image; ?>" alt="" style="height:150px;">
+							<?php echo $row->username; ?>
+						</a>
+					</div>
+					<?php
+					}
+					?>
+				</div>
+			</div>
+		
+<?php require "footer.php"; ?>
